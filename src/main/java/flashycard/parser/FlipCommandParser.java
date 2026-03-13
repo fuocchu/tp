0d2d28d398f4a503d82a88c453ebb5a6ddbd564c
Clement Ng <Clement.ng.62@outlook.com>
2026-03-13 13:03:20 +0800

    implement parsing for flip

---
+++ src/main/java/flashycard/parser/FlipCommandParser.java
@@ -0,0 +1,28 @@
+package flashycard.parser;
+
+import java.util.regex.Matcher;
+
+import flashycard.command.Command;
+import flashycard.command.DummyCommand;
+import flashycard.exceptions.InvalidArgumentException;
+
+public class FlipCommandParser extends CommandParser {
+    public FlipCommandParser() {
+        super("flip", "(?<id>.+?)");
+    }
+
+    @Override
+    public Command parse(String fullCommand) throws InvalidArgumentException {
+        Matcher matches = this.match(fullCommand);
+
+        try {
+            String idStr = matches.group("id").trim();
+            int id = Integer.parseInt(idStr);
+            return new DummyCommand(id); // TODO: return the correct class
+        } catch (NumberFormatException e) {
+            throw new InvalidArgumentException("Invalid ID given: ID must be a number");
+        }
+
+    }
+
+}
---
+++ src/test/java/flashycard/parser/FlipCommandParserTest.java
@@ -0,0 +1,44 @@
+package flashycard.parser;
+
+import flashycard.command.Command;
+import flashycard.exceptions.InvalidArgumentException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FlipCommandParserTest {
+
+    @Test
+    void testParse_validId_returnsCommand() throws InvalidArgumentException {
+        FlipCommandParser parser = new FlipCommandParser();
+        Command command = parser.parse("flip 42");
+        // TODO: Change to specific command type
+        assertTrue(command instanceof Command);
+    }
+
+    @Test
+    void testParse_validIdWithSpaces_returnsCommand() throws InvalidArgumentException {
+        FlipCommandParser parser = new FlipCommandParser();
+        Command command = parser.parse("flip    7   ");
+        // TODO: Change to specific command type
+        assertTrue(command instanceof Command);
+    }
+
+    @Test
+    void testParse_invalidId_throwsException() {
+        FlipCommandParser parser = new FlipCommandParser();
+        assertThrows(InvalidArgumentException.class, () -> parser.parse("flip abc"));
+    }
+
+    @Test
+    void testParse_missingId_throwsException() {
+        FlipCommandParser parser = new FlipCommandParser();
+        assertThrows(InvalidArgumentException.class, () -> parser.parse("flip "));
+    }
+
+    @Test
+    void testParse_emptyString_throwsInvalidArgumentException() {
+        FlipCommandParser parser = new FlipCommandParser();
+        assertThrows(InvalidArgumentException.class, () -> parser.parse(""));
+    }
+}
