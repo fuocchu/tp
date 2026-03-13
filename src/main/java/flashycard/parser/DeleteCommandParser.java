ec75f8d107eb9f62221ca171d40c67edaa198cd7
Clement Ng <Clement.ng.62@outlook.com>
2026-03-13 13:03:00 +0800

    implement parsing for delete

---
+++ src/main/java/flashycard/parser/DeleteCommandParser.java
@@ -0,0 +1,28 @@
+package flashycard.parser;
+
+import java.util.regex.Matcher;
+
+import flashycard.command.Command;
+import flashycard.command.DummyCommand;
+import flashycard.exceptions.InvalidArgumentException;
+
+public class DeleteCommandParser extends CommandParser {
+    public DeleteCommandParser() {
+        super("delete", "(?<id>.+?)");
+    }
+
+    @Override
+    public Command parse(String fullCommand) throws InvalidArgumentException {
+        Matcher matches = this.match(fullCommand);
+
+        try {
+            String idStr = matches.group("id").trim();
+            int id = Integer.parseInt(idStr);
+            return new DummyCommand(id); // TODO: return the correct class
+        } catch (NumberFormatException e) {
+            throw new InvalidArgumentException("Invalid ID given: ID must be a number");
+        }
+
+    }
+
+}
---
+++ src/test/java/flashycard/parser/DeleteCommandParserTest.java
@@ -0,0 +1,44 @@
+package flashycard.parser;
+
+import flashycard.command.Command;
+import flashycard.exceptions.InvalidArgumentException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DeleteCommandParserTest {
+
+    @Test
+    void testParse_validId_returnsCommand() throws InvalidArgumentException {
+        DeleteCommandParser parser = new DeleteCommandParser();
+        Command command = parser.parse("delete 42");
+        // TODO: Change to specific command type
+        assertTrue(command instanceof Command);
+    }
+
+    @Test
+    void testParse_validIdWithSpaces_returnsCommand() throws InvalidArgumentException {
+        DeleteCommandParser parser = new DeleteCommandParser();
+        Command command = parser.parse("delete    7   ");
+        // TODO: Change to specific command type
+        assertTrue(command instanceof Command);
+    }
+
+    @Test
+    void testParse_invalidId_throwsException() {
+        DeleteCommandParser parser = new DeleteCommandParser();
+        assertThrows(InvalidArgumentException.class, () -> parser.parse("delete abc"));
+    }
+
+    @Test
+    void testParse_missingId_throwsException() {
+        DeleteCommandParser parser = new DeleteCommandParser();
+        assertThrows(InvalidArgumentException.class, () -> parser.parse("delete "));
+    }
+
+    @Test
+    void testParse_emptyString_throwsInvalidArgumentException() {
+        DeleteCommandParser parser = new DeleteCommandParser();
+        assertThrows(InvalidArgumentException.class, () -> parser.parse(""));
+    }
+}
