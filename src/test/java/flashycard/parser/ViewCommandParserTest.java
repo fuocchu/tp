97b2ad87e6d321f63dceccb5df8f4104819a95b2
Clement Ng <Clement.ng.62@outlook.com>
2026-03-13 13:03:51 +0800

    implement parsing for view

---
+++ src/test/java/flashycard/parser/ViewCommandParserTest.java
@@ -0,0 +1,44 @@
+package flashycard.parser;
+
+import flashycard.command.Command;
+import flashycard.exceptions.InvalidArgumentException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ViewCommandParserTest {
+
+    @Test
+    void testParse_validId_returnsCommand() throws InvalidArgumentException {
+        ViewCommandParser parser = new ViewCommandParser();
+        Command command = parser.parse("view 42");
+        // TODO: Change to specific command type
+        assertTrue(command instanceof Command);
+    }
+
+    @Test
+    void testParse_validIdWithSpaces_returnsCommand() throws InvalidArgumentException {
+        ViewCommandParser parser = new ViewCommandParser();
+        Command command = parser.parse("view    7   ");
+        // TODO: Change to specific command type
+        assertTrue(command instanceof Command);
+    }
+
+    @Test
+    void testParse_invalidId_throwsException() {
+        ViewCommandParser parser = new ViewCommandParser();
+        assertThrows(InvalidArgumentException.class, () -> parser.parse("view abc"));
+    }
+
+    @Test
+    void testParse_missingId_throwsException() {
+        ViewCommandParser parser = new ViewCommandParser();
+        assertThrows(InvalidArgumentException.class, () -> parser.parse("view "));
+    }
+
+    @Test
+    void testParse_emptyString_throwsInvalidArgumentException() {
+        ViewCommandParser parser = new ViewCommandParser();
+        assertThrows(InvalidArgumentException.class, () -> parser.parse(""));
+    }
+}
