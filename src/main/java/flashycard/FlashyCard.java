dcadf90afd30552e1e8b853b5b358e1c2acdcfde
cyanduck32 <msproton858@gmail.com>
2026-03-18 20:46:05 +0800

    update app entry point

--- src/main/java/flashycard/FlashyCard.java
+++ src/main/java/flashycard/FlashyCard.java
@@ -1,6 +1,11 @@
 package flashycard;
 
+import flashycard.command.Command;
+import flashycard.exceptions.CorruptedDataException;
+import flashycard.exceptions.InvalidArgumentException;
+import flashycard.exceptions.InvalidCommandException;
 import flashycard.model.KnowledgeBase;
+import flashycard.parser.Parser;
 import flashycard.storage.Storage;
 import flashycard.ui.Ui;
 
@@ -10,11 +15,34 @@ public class FlashyCard {
     private KnowledgeBase knowledgeBase;
 
     public FlashyCard(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            knowledgeBase = storage.load();
+        } catch (CorruptedDataException e){
+            ui.showError("Data file corrupted. starting with an empty base.");
+            knowledgeBase = new KnowledgeBase();
+        }
     }
 
-    public void run() {
+    public void run(){
+        ui.showWelcome();
+        boolean isExit = false;
+
+        while (!isExit){
+            try {
+                String fullCommand = ui.readCommand();
+                Command c = Parser.parse(fullCommand);
+                c.execute(knowledgeBase, ui, storage);
+                isExit = c.isExit();
+            } catch (InvalidCommandException e){
+                ui.showError(e.getMessage());
+            }
+        }
+        ui.showExitMessage();
     }
 
     public static void main(String[] args) {
+        new FlashyCard("data/flashcards.txt").run();
     }
 }
