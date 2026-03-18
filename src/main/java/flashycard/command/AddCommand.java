d1d57d1a8aa6100bcfe76f44831b2c54e6a81f21
cyanduck32 <msproton858@gmail.com>
2026-03-19 00:42:04 +0800

    fixing AddCommand

--- src/main/java/flashycard/command/AddCommand.java
+++ src/main/java/flashycard/command/AddCommand.java
@@ -1,4 +1,24 @@
 package flashycard.command;
 
-public class AddCommand {
+import flashycard.model.Card;
+import flashycard.model.KnowledgeBase;
+import flashycard.storage.Storage;
+import flashycard.ui.Ui;
+
+public class AddCommand extends Command {
+    private final String question;
+    private final String answer;
+
+    public AddCommand(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    @Override
+    public void execute(KnowledgeBase cards, Ui ui, Storage storage) {
+        Card card = new Card(question, answer);
+        cards.addCard(card);
+        storage.save(cards);
+        ui.showAddedMessage(card);
+    }
 }
